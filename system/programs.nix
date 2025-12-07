dad0b9f496eb5625628aaf46d82b35fa00b9c320
Sternwarts1881 <dbinbogafbs@gmail.com>
2025-12-07 20:28:21 +0100

    added winboat + docker

--- system/programs.nix
+++ system/programs.nix
@@ -122,4 +122,22 @@ in {
       winboat
     ];
   };
+  virtualisation={
+    docker = {
+      enable = true;
+      # Set up resource limits
+      daemon.settings = {
+        experimental = true;
+        default-address-pools = [
+          {
+            base = "172.30.0.0/16";
+            size = 24;
+          }
+        ];
+      };
+    };
+  };
+
+
+
 }
